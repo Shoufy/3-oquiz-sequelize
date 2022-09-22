bd7cc3fd1fac4362bbd09d9133f0f361d1e1a69b
shoufy <sophie@duck.com>
2022-09-23 00:49:19 +0200

    create User model with Sequelize

---
+++ app/models/User.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../dataBase');
+
+class User extends Model {
+
+}
+
+User.init({
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    tableName: "user",
+    timestamps: false
+})
+
+module.exports = User;
